f0f484770606e5c93fd4ff1e558a2df992cacb57
Ezhil-04 <161230108+Ezhil-04@users.noreply.github.com>
2024-03-13 11:45:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,53 @@ addbtn.addEventListener("click",addnote)
 getItem()
 function addnote()
 {
-  
-    const note=document.createElement("div")
-    note.classList.add("boxes")
-    note.style.overflow="auto"
-box2.appendChild(note)
-    let text=textnote.value;
-    note.textContent=text;
-    textnote.value=""
-    savedata();
+
+    if(textnote.value=="")
+    { alert("write something to add")
+   
+    }
+    else{  
+ 
+        const paragraph=document.createElement("p");
+        paragraph.setAttribute("contenteditable","true")
+        paragraph.id="para"
+        let text=textnote.value;
+        let textnode=document.createTextNode(text)
+        paragraph.appendChild(textnode)
+    
+      
+        textnote.value=""
+        paragraph.classList.add("boxes")
+        paragraph.style.overflow="auto"
+    box2.appendChild(paragraph)
+    const del=document.createElement("img")
+    del.src="pic/trash-bin.png"
+    del.id="delete"
+    paragraph.appendChild(del)
+        
+       
+        savedata();
+    }
+     
 
 }
+
+box2.addEventListener("click",(e)=>{
+    if(e.target.tagName==="IMG")
+    {
+        e.target.parentElement.remove();
+        updatestorage()
+    }
+    if(e.target.tagName==="P"){
+        let curbox=document.querySelectorAll(".boxes")
+    curbox.forEach(l =>{
+        l.onkeyup=function(){
+            updatestorage()
+        }
+
+      })
+      }})
+
 
 function savedata()
 {
@@ -24,5 +60,17 @@ function savedata()
 
 function getItem()
 {
-    box2.value=localStorage.getItem("data")
+    box2.innerHTML=localStorage.getItem("data")
 }
+
+function updatestorage(){
+    localStorage.setItem("data",box2.innerHTML)
+}
+
+document.addEventListener("keydown",e=>{
+    if(e.key==="Enter"){
+        document.execCommand("insertLineBreak")
+        e.preventDefault();
+    }
+})
+box2.removeChild()
